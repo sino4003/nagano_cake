6715cca7bd71940bf7881b58f1f95682675c3529
EC2 Default User <ec2-user@ip-172-31-3-217.ap-northeast-1.compute.internal>
2023-05-14 16:27:10 +0000

    finish

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,15 +9,17 @@ class Admin::OrdersController < ApplicationController
     @order_detail = @order.order_details
   end
 
-   def update
-  @order = Order.find(params[:id])
-  @order_details = OrderDetail.where(order_id: params[:id])
-  if @order.update(order_params)
-    @order_details.update_all(making_status: 1) if @order.status == "入金確認"
-    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
-  end
-  redirect_to admin_order_path(@order)
-end
+    def update
+		order = Order.find(params[:id])
+		order_details = order.order_details
+    order.update(order_params)
+
+		if order.status == "入金確認"
+			order_details.update_all(making_status: "製作待ち")
+		end
+		redirect_to admin_order_path(order.id)
+	end
+
 
   private
 
